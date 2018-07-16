11e8b2b6802ea5dfd9e4e4cd2cbab67a31576f50
shaheemMPM <shaheem.mpm@gmail.com>
2018-07-16 19:39:34 +0530

    js finished

---
+++ scripts/local.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+  var btc;
+
+
+  $.ajax({
+    url : 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=INR',
+    success : function(result) {
+      btc = result.INR;
+      $('#inr-ip').val(btc);
+    }
+  });
+
+
+
+
+  $('#btc-ip').keyup(function() {
+    var x = $('#btc-ip').val();
+    // var val = Number(x);
+    var rv = x*btc;
+    $('#inr-ip').val(rv);
+  });
+
+  $('#inr-ip').keyup(function() {
+    var x = $('#inr-ip').val();
+    // var val = Number(x);
+    var rv = x/btc;
+    $('#btc-ip').val(rv);
+  })
+
+
+
+});
